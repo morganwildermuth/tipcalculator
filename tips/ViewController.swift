b55efcec06028510878867327ebc19b8f031ad8f
Morgan Wildermuth <morgan.wildermuth@gmail.com>
2015-08-04 20:51:40 -0700

    fixed bug

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -45,27 +45,7 @@ class ViewController: UIViewController {
         println("location start")
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        tipLabel.text = "$0.00"
-        totalLabel.text = "$0.00"
-        setDefaultTip()
-    }
-    
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        setDefaultTip()
-        setDetailsPanelStatus()
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    @IBAction func onEditingChanged(sender: AnyObject) {
-        setDetailsPanelStatus()
+    private func calculateTotal(){
         var tipPercentages = [0.18, 0.2, 0.22]
         var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
 
@@ -78,6 +58,31 @@ class ViewController: UIViewController {
 
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        setDefaultTip()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        setDefaultTip()
+        setDetailsPanelStatus()
+        calculateTotal()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        setDetailsPanelStatus()
+        calculateTotal()
     }
 
     @IBAction func onTap(sender: AnyObject) {
