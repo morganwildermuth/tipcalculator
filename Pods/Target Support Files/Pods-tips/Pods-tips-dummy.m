0b8014835d749163e28ed38f390356ac4e422061
Morgan Wildermuth <morgan.wildermuth@gmail.com>
2015-08-04 21:41:15 -0700

    added CocoaPods + Venmo

---
+++ Pods/Target Support Files/Pods-tips/Pods-tips-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_tips : NSObject
+@end
+@implementation PodsDummy_Pods_tips
+@end
--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Venmo_iOS_SDK
 
 class ViewController: UIViewController {
 
